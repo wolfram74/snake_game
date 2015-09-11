3bd66b600b112d798914690514a9c5504fa65d9d
wolfram74 <phaugen@gmail.com>
2015-09-11 08:21:45 -0700

    first draft on psudeo code

---
+++ js/source.js
@@ -0,0 +1,18 @@
+/*
+psudeo code
+gameplay description of snake:
+  a user takes control of a snake object that continues in a straightline until given a new direction
+  snakes can eat apples, when a snake eats an apple it lengthens the snakes body
+  if a snake overlaps with it's body, it dies.
+
+object description
+snakes have 
+  position
+  velocity
+  body
+apples have 
+  position
+game space has
+  width
+  height
+*/
