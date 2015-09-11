04460cb87e8e8e5d0ad317bd1279fb8b79920727
wolfram74 <phaugen@gmail.com>
2015-09-11 08:37:44 -0700

    wrote tests and implemented direction choosing functionality for snake

--- jasmine-standalone-2.2.0/spec/SourceSpec.js
+++ jasmine-standalone-2.2.0/spec/SourceSpec.js
@@ -22,4 +22,17 @@ describe("Snake", function(){
     snake.timeStep()
     expect(oldPosition).not.toEqual(snake.position)
   })
+
+  it("should respond to key input", function(){
+    var snake = new Snake()
+    expect(snake.velocity).toEqual([1,0])
+    snake.course("W")
+    expect(snake.velocity).toEqual([0,1])
+    snake.course("A")
+    expect(snake.velocity).toEqual([-1,0])
+    snake.course("S")
+    expect(snake.velocity).toEqual([0,-1])
+    snake.course("D")
+    expect(snake.velocity).toEqual([1,0])
+  })
 })
--- js/snakeClass.js
+++ js/snakeClass.js
@@ -11,3 +11,14 @@ Snake.prototype.timeStep = function(){
   };
   this.position = newPosition
 };
+
+Snake.prototype.course = function(key){
+  this.directions[key].bind(this)()
+}
+
+Snake.prototype.directions = {
+  W: function(){this.velocity = [0,1]},
+  A: function(){this.velocity = [-1,0];console.log(this)},
+  S: function(){this.velocity = [0,-1]},
+  D: function(){this.velocity = [1,0]}
+}
